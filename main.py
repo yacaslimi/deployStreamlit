0740b6024e8c1ef9c336e9c779085ad705c4ec48
yacaslimi <105648250+yacaslimi@users.noreply.github.com>
2023-12-12 22:32:44 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,40 @@
 import streamlit as st
 import joblib
-import pandas
-import sklearn
-
+import numpy as np
 
 # Load your model and label encoder
 svm_model = joblib.load('svm_model.pkl')
 le = joblib.load('label_encoder.pkl')
+
 def main():
     st.title("ML Model Prediction")
-    # Creating text input boxes for each feature
-    age = st.number_input("Age", min_value=0.5, max_value=100.0, step=0.1)
-    cataracte_unilaterale = st.selectbox("Cataracte Unilatérale", [0, 1])  # Assuming binary input: 0 for No, 1 for Yes
-    cataracte_bilaterale = st.selectbox("Cataracte Bilatérale", [0, 1])
-    opacite_cornee = st.selectbox("Opacité Cornée", [0, 1])
-    atteinte_retine = st.selectbox("Atteinte Rétine", [0, 1])
-    # Button to make prediction
-    if st.button('Predict'):
-        # Prepare the feature vector for prediction
-        features = [age, cataracte_unilaterale, cataracte_bilaterale, opacite_cornee, atteinte_retine]
-        
-        # Make a prediction
-        prediction = svm_model.predict([features])
-        prediction_label = le.inverse_transform(prediction)
+
+    with st.form("prediction_form"):
+        st.markdown("### Please enter the patient details:")
+
+        # Using columns for a better layout
+        col1, col2 = st.columns(2)
+
+        with col1:
+            age = st.slider("Age", min_value=0.5, max_value=100.0, step=0.1, value=30.0)
+            cataracte_unilaterale = st.selectbox("Cataracte Unilatérale", [0, 1])
+
+        with col2:
+            cataracte_bilaterale = st.selectbox("Cataracte Bilatérale", [0, 1])
+            opacite_cornee = st.selectbox("Opacité Cornée", [0, 1])
+            atteinte_retine = st.selectbox("Atteinte Rétine", [0, 1])
+
+        submitted = st.form_submit_button("Predict")
+        if submitted:
+            # Prepare the feature vector for prediction
+            features = np.array([age, cataracte_unilaterale, cataracte_bilaterale, opacite_cornee, atteinte_retine]).reshape(1, -1)
+            
+            # Make a prediction
+            prediction = svm_model.predict(features)
+            prediction_label = le.inverse_transform(prediction)
+            
+            # Display the result
+            st.markdown(f"#### Prediction: {prediction_label[0]}")
+
+if __name__ == '__main__':
+    main()
