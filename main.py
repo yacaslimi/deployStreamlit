597c656b682e8e5e501603b7ea7deceb7a6f9772
yacaslimi <105648250+yacaslimi@users.noreply.github.com>
2023-12-12 22:12:19 +0100

    Update main.py

--- main.py
+++ main.py
@@ -10,37 +10,30 @@ le = joblib.load('label_encoder.pkl')
 def main():
     st.title("ML Model Prediction")
 
-    with st.form("prediction_form"):
-        st.markdown("### Please enter the patient details:")
+    # Creating text input boxes for each feature
+    age = st.number_input("Age", min_value=0.5, max_value=100.0, step=0.1)
+    cataracte_unilaterale = st.selectbox("Cataracte Unilatérale", [0, 1])  # Assuming binary input: 0 for No, 1 for Yes
+    cataracte_bilaterale = st.selectbox("Cataracte Bilatérale", [0, 1])
+    opacite_cornee = st.selectbox("Opacité Cornée", [0, 1])
+    atteinte_retine = st.selectbox("Atteinte Rétine", [0, 1])
 
-        # Using columns for a better layout
-        col1, col2 = st.columns(2)
+    # Button to make prediction
+    if st.button('Predict'):
+        # Prepare the feature vector for prediction
+        features = [age, cataracte_unilaterale, cataracte_bilaterale, opacite_cornee, atteinte_retine]
+        
+        # Make a prediction
+        prediction = svm_model.predict([features])
+        prediction_label = le.inverse_transform(prediction)
 
-        with col1:
-            age = st.slider("Age", min_value=0.5, max_value=100.0, step=0.1, value=30.0)
-            cataracte_unilaterale = st.selectbox("Cataracte Unilatérale", [0, 1])
+        # Determine the text to display based on the prediction
+        if prediction_label[0] == 1:
+            result_text = "Le patient a le gène PAX6"
+        else:
+            result_text = "Le patient n'a pas le gène PAX6"
 
-        with col2:
-            cataracte_bilaterale = st.selectbox("Cataracte Bilatérale", [0, 1])
-            opacite_cornee = st.selectbox("Opacité Cornée", [0, 1])
-            atteinte_retine = st.selectbox("Atteinte Rétine", [0, 1])
-
-        submitted = st.form_submit_button("Predict")
-        if submitted:
-            # Prepare the feature vector for prediction
-            features = [age, cataracte_unilaterale, cataracte_bilaterale, opacite_cornee, atteinte_retine]
-            
-            # Make a prediction
-            prediction = svm_model.predict([features])
-            prediction_label = le.inverse_transform(prediction)
-
-            # Debugging: Print statements to check the prediction
-            st.write("Prediction (numerical):", prediction)  # Debugging line
-            st.write("Prediction label:", prediction_label)  # Debugging line
-
-            # Display the result with custom styling
-            result_text = "Le patient a le gène PAX6" if prediction_label[0] == 1 else "Le patient n'a pas le gène PAX6"
-            st.markdown(f"<h2 style='color:red;'>{result_text}</h2>", unsafe_allow_html=True)
+        # Display the result
+        st.write(f"Prediction: {result_text}")
 
 if __name__ == '__main__':
     main()
